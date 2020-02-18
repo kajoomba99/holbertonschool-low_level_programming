5ad3b651863caaa4aeab45a49939ef46e66a7f80
Steven Brand <steven.brand1999@gmail.com>
2020-02-18 19:59:50 +0000

    7-puts_half.c correction

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,15 @@ void puts_half(char *str)
 	int i = 0, j, l;
 
 	while (str[i] != '\0')
-	{
 		i++;
-	}
 
 	l = i;
 
 	if ((i % 2) != 0)
 		i--;
 
-	for (j = (i / 2); j < l; j++)
-	{
+	for (j = (i / 2); j <= l; j++)
 		_putchar(str[j]);
-	}
+
 	_putchar('\n');
 }

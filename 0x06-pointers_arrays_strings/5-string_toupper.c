4362ae9da2cdf9036f20ecf173e732664f12f4a1
Steven Brand <steven.brand1999@gmail.com>
2020-02-20 22:36:52 +0000

    task number 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @s: char array.
+ * Return: the string converted to upper.
+ */
+char *string_toupper(char *s)
+{
+	int l, i;
+
+	while (s[l] != '\0')
+		l++;
+
+	for (i = 0; i < l;  i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = (s[i] - 32);
+	}
+
+	return (s);
+}

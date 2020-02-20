01e00c6d094910777247ef88769d4c43b9edbfb6
Steven Brand <steven.brand1999@gmail.com>
2020-02-20 18:41:35 +0000

    betty correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,19 @@
 #include "holberton.h"
-
+/**
+ * _strncat -  concatenates two strings.
+ * @dest: dest.
+ * @src: src.
+ * @n: n.
+ * Return: dest.
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0 , j, l = 0;
+	int i = 0, j, l = 0;
+
 	while (src[l] != '\0')
 		l++;
 
-	if (n > (l-1))
+	if (n > (l - 1))
 		n = l;
 
 	while (dest[i] != '\0')

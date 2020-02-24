3cab071be46ba8f96c502d62e851b3e4d76fae07
Steven Brand <steven.brand1999@gmail.com>
2020-02-24 20:29:44 +0000

    betty 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
 #include "holberton.h"
-
+/**
+ * _strchr - locates a character in a string.
+ * @s: String.
+ * @c: String.
+ * Return: a pointer to the first occurrence of the character c
+ * in the string s, or NULL if the character is not found.
+ */
 char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;

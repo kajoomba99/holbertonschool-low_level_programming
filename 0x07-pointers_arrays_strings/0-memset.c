5b8314bc7774840da94ad4d0adcc099789554bf3
Steven Brand <steven.brand1999@gmail.com>
2020-02-24 18:22:36 +0000

    Corrections

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,7 +8,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i, l = 0;
+
+	while (s[l])
+		l++;
+
+	if (n > (l - 1))
+		n = l;
 
 	for (i = 0; i < n; i++)
 	{

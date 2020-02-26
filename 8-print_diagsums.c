a2af6383f84287f95272b28e610344a47433c309
kajoomba99 <steven.brand1999@gmail.com>
2020-02-26 02:18:30 +0000

    8-print_diagsums.c

---
+++ 8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+
+void print_diagsums(int *a, int size)
+{
+	int sumd1 = 0, d1;
+	int sumd2 = 0, d2;
+
+	for (d1 = 0; d1 < (size * size); d1 += (size + 1))
+	{
+		sumd1 += a[d1];
+	}
+
+	for (d2 = (size - 1); d2 <= ((size * size) - size); d2 += (size - 1))
+	{
+		sumd2 += a[d2];
+	}
+
+	printf("%d, %d\n", sumd1, sumd2);
+}

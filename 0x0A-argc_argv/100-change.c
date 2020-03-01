9542bb5acaef388d97b17362e3d0865a6225fe67
kajoomba99 <steven.brand1999@gmail.com>
2020-03-01 22:37:11 +0000

    Corrections

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,32 +10,39 @@ int main(int argc __attribute__((unused)),
 	char *argv[] __attribute__((unused)))
 {
 	int c;
-	int cr = 0, count = 0;
+	int cr = 0, count = 0, rv = 0;
 
-	if (argc > 1)
+	if (argc == 2)
 	{
 		c = atoi(argv[1]);
-
-		while (cr <= c)
+		if (c > 0)
 		{
-			if ((cr + 25) <= c)
-				cr += 25;
-			else if ((cr + 10) <= c)
-				cr += 10;
-			else if ((cr + 5) <= c)
-				cr += 5;
-			else if ((cr + 2) <= c)
-				cr += 2;
-			else
-				cr += 1;
+			while (cr <= c)
+			{
+				if ((cr + 25) <= c)
+					cr += 25;
+				else if ((cr + 10) <= c)
+					cr += 10;
+				else if ((cr + 5) <= c)
+					cr += 5;
+				else if ((cr + 2) <= c)
+					cr += 2;
+				else
+					cr += 1;
 
-			count += 1;
+				count += 1;
+			}
+			printf("%i\n", count - 1);
 		}
-		printf("%i\n", count - 1);
+		else
+		{
+			printf("0\n");
+		}
 	}
 	else
 	{
 		printf("Error\n");
+		rv = 1;
 	}
-	return (0);
+	return (rv);
 }

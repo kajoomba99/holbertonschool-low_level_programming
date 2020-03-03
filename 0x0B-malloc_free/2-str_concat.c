deb0c927abbc52e5c0bd91c4f2eb73cb2becad9d
Steven Brand <steven.brand1999@gmail.com>
2020-03-03 22:32:24 +0000

    2-str_concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * get_size - get_size.
+ * @c: char pointer.
+ * Return: lenght of string c.
+ */
+int get_size(char *c)
+{
+	int i;
+
+	while (c[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * str_concat - str_concat
+ * @s1: s1
+ * @s2: s2
+ * Return: return.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int lenght, i = 0, j = 0;
+
+	char *p, *empty = "";
+
+	if (s1 == NULL)
+		s1 = empty;
+
+	if (s2 == NULL)
+		s2 = empty;
+
+	lenght = get_size(s1) + get_size(s2);
+
+	p = malloc((lenght + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (NULL);
+
+	while (s1[i])
+	{
+		p[i] = s1[i];
+		i++;
+	}
+
+	while (s2[j])
+        {
+                p[i] = s2[j];
+		j++;
+                i++;
+        }
+
+	p[i] = '\0';
+
+	return (p);
+}

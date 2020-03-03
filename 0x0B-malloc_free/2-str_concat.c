9517b1942c68078c86a29a99648a0736b4fcc44c
Steven Brand <steven.brand1999@gmail.com>
2020-03-03 23:17:00 +0000

    Correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,19 +24,23 @@ int get_size(char *c)
  */
 char *str_concat(char *s1, char *s2)
 {
-	int lenght, i = 0, j = 0;
+	int lenght, i = 0, j = 0, str1 = 0, str2 = 0;
 
-	char *p, *emp;
+	char *p, *empty;
 
-	emp = "";
+	empty = "";
 
 	if (s1 == NULL)
-		s1 = emp;
+		s1 = empty;
+	else
+		str1 = get_size(s1);
 
 	if (s2 == NULL)
-		s2 = emp;
+		s2 = empty;
+	else
+		str2 = get_size(s2);
 
-	lenght = get_size(s1) + get_size(s2);
+	lenght = str1 + str2;
 
 	p = malloc((lenght + 1) * sizeof(char));
 

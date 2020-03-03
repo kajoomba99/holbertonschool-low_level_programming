d5fb663b223bfdf1d8ec1cd54b8b415288b622f9
Steven Brand <steven.brand1999@gmail.com>
2020-03-03 22:50:27 +0000

    Correction 2-str_cat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,15 +26,17 @@ char *str_concat(char *s1, char *s2)
 {
 	int lenght, i = 0, j = 0;
 
-	char *p, *empty = "";
+	char *p;
 
 	if (s1 == NULL)
-		s1 = malloc(sizeof(char) + 1);
+	{
+		s1 = malloc(sizeof(char) * 1);
 		*s1 = '\0';
+	}
 
 	if (s2 == NULL)
 	{
-		s2 = malloc(sizeof(char) + 1);
+		s2 = malloc(sizeof(char) * 1);
 		*s2 = '\0';
 	}
 

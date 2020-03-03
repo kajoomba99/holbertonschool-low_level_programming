fe2fece0ff7acf5b37312b4d2103177b239484f6
Steven Brand <steven.brand1999@gmail.com>
2020-03-03 23:04:14 +0000

    Corrections

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,16 +29,10 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 
 	if (s1 == NULL)
-	{
-		s1 = malloc(sizeof(char) * 1);
-		*s1 = '\0';
-	}
+		s1 = "";
 
 	if (s2 == NULL)
-	{
-		s2 = malloc(sizeof(char) * 1);
-		*s2 = '\0';
-	}
+		s2 = "";
 
 	lenght = get_size(s1) + get_size(s2);
 

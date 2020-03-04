7258625161e8ed2493b387876f9c9d2b05f75f54
Steven Brand <steven.brand1999@gmail.com>
2020-03-04 01:22:52 +0000

    task number 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,10 +2,30 @@
 
 int **alloc_grid(int width, int height)
 {
-	int *p1, **p2;
-	p1 = malloc(sizeof(int) * (width * heihgt));
-	if (p1 == NULL)
+	int **p;
+	int i;
+
+	if (width < 0 || height < 0)
 		return (NULL);
 
-	p2 = p1;
+	p = malloc(sizeof(p) * height);
+
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(sizeof(int) * width);
+
+		if (p[i] == NULL)
+		{
+			free(p[i]);
+			return (NULL);
+		}
+	}
+
+	return (p);
 }

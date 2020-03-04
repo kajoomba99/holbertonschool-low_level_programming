f30725c7de07f18189d132fd3b732f41a9457a07
Steven Brand <steven.brand1999@gmail.com>
2020-03-04 21:25:27 +0000

    4th point

---
+++ 0x0B-malloc_free/.#3-main.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.2633:1583275123
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include<stdlib.h>
+/**
+ * free_grid - free_grid
+ * @grid: grid
+ * @height: height
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}

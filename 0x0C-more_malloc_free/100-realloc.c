96d3b2fa863e260bc53637757d109a8b3d4bfec6
Steven Brand <steven.brand1999@gmail.com>
2020-03-05 23:55:54 +0000

    task #4 advanced

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+/**
+ * _memcpy - _memcpy
+ * @dest: dest
+ * @src: src
+ * @n: n
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
+
+/**
+ * _realloc - _realloc
+ * @ptr: ptr
+ * @old_size: old_size
+ * @new_size: new_size
+ * Return: pointer
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	p = malloc(new_size);
+	_memcpy(p, ptr, old_size);
+	free(ptr);
+
+	return (p);
+}

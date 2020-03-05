18436ce8e7528ae8141f761317de8cc961647195
kajoomba99 <steven.brand1999@gmail.com>
2020-03-05 02:18:36 +0000

    Fifth point

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * argstostr - argstostr
+ * @ac: ac
+ * @av: av
+ * Return: c
+ */
+char *argstostr(int ac, char **av)
+{
+	int acum = 0, i, l, p = 0;
+	char *c;
+
+	for (i = 0; i < ac; i++)
+	{
+		acum += strlen(av[i]);
+	}
+	c = malloc((acum + 1) * sizeof(char));
+	if (c == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (l = 0; av[i][l] != '\0'; l++)
+		{
+			c[p] = av[i][l];
+			p++;
+		}
+		c[p] = '\n';
+		p++;
+	}
+	return (c);
+}

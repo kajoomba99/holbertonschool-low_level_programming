063461115f395853758ed89fdff3c8134ada772e
Steven Brand <steven.brand1999@gmail.com>
2020-03-09 22:27:42 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+#ifndef STEVEN_H
+#define STEVEN_H
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+#endif

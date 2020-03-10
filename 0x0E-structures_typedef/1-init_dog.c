0bfce0fba006ca28404181c73e9d93312e8c54a6
Steven Brand <steven.brand1999@gmail.com>
2020-03-10 00:31:12 +0000

    1-ini_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,12 @@
 #include "dog.h"
 #include <stdlib.h>
+/**
+ * init_dog - init_dog
+ * @d: d
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -5,6 +5,8 @@ void free_dog(dog_t *d)
 {
 	if (d)
 	{
+		free(d->name);
+		free(d->owner);
 		free(d);
 	}
 }

09d55b6947104926c7eee5b7517018ae80a7a6e3
kajoomba99 <steven.brand1999@gmail.com>
2020-03-19 21:05:56 +0000

    Update

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - free_list
+ * @head: head
+ */
+void free_list(list_t *head)
+{
+	list_t *p, *store;
+
+	p = head;
+
+	while (p != NULL)
+	{
+		store = p->next;
+		free(p);
+		p = store;
+	}
+}

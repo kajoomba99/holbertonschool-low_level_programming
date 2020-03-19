e6da7ba202b803814f6313c79dedcda5a8a8b831
kajoomba99 <steven.brand1999@gmail.com>
2020-03-19 21:46:17 +0000

    Updates

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,9 @@
 #include "lists.h"
-
-void myStartupFun (void)
-{ 
-    printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n"); 
-} 
+/**
+ * myStartupFun - myStartupFun
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

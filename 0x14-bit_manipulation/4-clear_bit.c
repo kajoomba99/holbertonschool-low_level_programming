aeaca756cd93363a168512aa5695b62a1bced408
kajoomba99 <steven.brand1999@gmail.com>
2020-03-26 23:52:35 +0000

    updates

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * clear_bit - clear_bit
+ * @n: n
+ * @index: index
+ * Return: return
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int ref;
+
+	if (index > 63)
+		return (-1);
+
+	ref = 1 << index;
+	*n -= ref;
+	return (1);
+}

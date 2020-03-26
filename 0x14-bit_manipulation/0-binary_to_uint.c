78b4132992d64808657bce9da7036f4376a4cf6c
kajoomba99 <steven.brand1999@gmail.com>
2020-03-26 19:49:01 +0000

    Update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,25 +8,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int bin = 1, acum = 0, index;
+	unsigned int acum = 0, index;
 
 	if (b == NULL)
 		return (0);
 
-	for (index = 1; b[index] != '\0'; index++)
-		bin *= 2;
-
-	index = 0;
-
 	for (index = 0; b[index] != '\0'; index++)
 	{
+		acum *= 2;
+
 		if (b[index] == '1')
-			acum += bin;
+			acum += 1;
 
 		if (b[index] != '1' && b[index] != '0')
 			return (0);
-
-		bin /= 2;
 	}
 
 	return (acum);

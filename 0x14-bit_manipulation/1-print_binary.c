0bb94333b99c66d435fffb3f63ee4e447939841d
kajoomba99 <steven.brand1999@gmail.com>
2020-03-26 21:05:53 +0000

    Updates

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * print_binary - print_binary
+ * @n: n
+ */
+void print_binary(unsigned long int n)
+{
+	int c, result, band = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	for (c = 31; c >= 0; c--)
+	{
+		result = n >> c;
+
+		if (result & 1)
+		{
+			_putchar('1');
+			band = 1;
+		}
+		else if (band != 0)
+		{
+			_putchar('0');
+		}
+	}
+}

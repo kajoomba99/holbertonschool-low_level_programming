7a34eedb9dfd61cc6bb14d5d34e0c8c28dc4817f
kajoomba99 <steven.brand1999@gmail.com>
2020-03-27 00:33:34 +0000

    Updates

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,20 +7,14 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	int ref, refn;
+	int ref;
 
 	if (index > 63)
 		return (-1);
 
-	refn = *n;
-
 	ref = 1 << index;
-	refn -= ref;
 
-	if (refn < 0)
-		*n = 0;
-	else
-		*n = refn;
+	*n = *n & (~ref);
 
 	return (1);
 }

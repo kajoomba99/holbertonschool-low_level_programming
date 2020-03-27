51aa895bfecf4febbb6048d72da571f811ad3ae6
kajoomba99 <steven.brand1999@gmail.com>
2020-03-27 01:27:12 +0000

    Updates

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * flip_bits - flip_bits
+ * @n: n
+ * @m: m
+ * Return: return
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int count = 0, z;
+
+	for (z = n ^ m; z != 0; z = z >> 1)
+		count += z & 1;
+
+	return (count);
+}

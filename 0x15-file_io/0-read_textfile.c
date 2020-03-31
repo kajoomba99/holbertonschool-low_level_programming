7161d87ff8a8c1adbc35556b60c1e1a3ec0c99b7
Steven Brand <steven.brand1999@gmail.com>
2020-03-31 15:04:35 -0500

    Correction task #0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,22 +16,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buf = malloc(letters);
+	buf = malloc(letters * sizeof(char));
 
 	if (buf == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 
-	if (fd == -1)
-		return (0);
-
 	rr = read(fd, buf, letters);
 
-	rw = write(STDIN_FILENO, buf, rr);
+	rw = write(STDOUT_FILENO, buf, rr);
 
-	if (rw == -1 || rr == -1)
+	if (rw == -1 || rr == -1 || fd == -1)
+	{
+		close(fd);
+		free(buf);
 		return (0);
+	}
 
 	close(fd);
 

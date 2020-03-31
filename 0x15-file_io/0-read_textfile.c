c73828bb0a0c22a8178436c9afd69ddcba0ffbfd
Steven Brand <steven.brand1999@gmail.com>
2020-03-31 14:58:53 -0500

    Betty correction in task #0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,9 @@
 
 /**
  * read_textfile - read_textfile
- * @filename: the name of the filecd
+ * @filename: the name of the file
+ * @letters: number of letters
+ * Return: the actual number of letters it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)

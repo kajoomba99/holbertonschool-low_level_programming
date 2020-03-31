6b734d254bc8a886ae9561470e2fc41d1ce3089a
Steven Brand <steven.brand1999@gmail.com>
2020-03-31 16:11:13 -0500

    Task #1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,12 +13,14 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1 || filename == NULL || text_content == NULL)
+	if (fd == -1 || filename == NULL)
 		return (-1);
 
-	while (text_content[len] != '\0')
-		len++;
-
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+	}
 
 	rw = write(fd, text_content, len);
 

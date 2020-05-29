59a512f8429fe80dead322ef9fcf6d86af39c9bb
kajoomba99 <steven.brand1999gmail.com>
2020-05-29 14:22:53 -0500

    Update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,13 +7,26 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = 0;
+	unsigned int slot = key_index((unsigned char *)key, ht->size);
 
-	if (ht == NULL || key == NULL)
+	hash_node_t *entry = ht->array[slot];
+
+	if (entry == NULL)
+	{
 		return (NULL);
-	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] != NULL)
-		return (ht->array[index]->value);
-	else
-		return (NULL);
+	}
+
+	while (entry != NULL)
+	{
+
+		if (strcmp(entry->key, key) == 0)
+		{
+			return (entry->value);
+		}
+
+
+		entry = entry->next;
+	}
+
+	return (NULL);
 }

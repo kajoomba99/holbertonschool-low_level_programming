33e72c73d66905f497b4bf5182039cb2792e9b69
Steven <steven.brand1999@gmail.com>
2020-07-03 22:35:17 -0500

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,41 +1,21 @@
 #!/usr/bin/python3
-"""
-Islanda Perimeter
-"""
-
-
-def verify_perimeter(grid, f, c):
-    """
-    Function to verify perimeter
-    """
-    perimeter = 0
-    filas = len(grid)
-    columnas = len(grid[0])
-    perimeter = 0
-    if grid[f][c] == 1:
-        if f - 1 >= 0 and grid[f - 1][c] == 0:
-            perimeter += 1
-        if c + 1 < columnas and grid[f][c + 1] == 0:
-            perimeter += 1
-        if f + 1 < filas and grid[f + 1][c] == 0:
-            perimeter += 1
-        if c - 1 >= 0 and grid[f][c - 1] == 0:
-            perimeter += 1
-    return perimeter
+""" Define an island perimeter """
 
 
 def island_perimeter(grid):
-    """
-    returns the perimeter of the island described in grid
-    """
-    if type(grid) != list or not grid:
-        return None
-    filas = len(grid)
-    columnas = len(grid[0])
-    perimeter = 0
+    """ Return the perimeter """
 
-    for f in range(filas):
-        for c in range(columnas):
-            perimeter += verify_perimeter(grid, f, c)
+    height = len(grid)
+    width = len(grid[0])
+    totalsquare = 0
+    edges = 0
 
-    return perimeter
+    for row in range(height):
+        for col in range(width):
+            if grid[row][col] == 1:
+                totalsquare += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    edges += 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    edges += 2
+    return (totalsquare - edges)

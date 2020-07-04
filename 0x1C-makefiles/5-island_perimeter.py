d34b0f509f235328abe0fb6a5a0917d3908b665a
Steven <steven.brand1999@gmail.com>
2020-07-03 22:31:55 -0500

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,26 @@ Islanda Perimeter
 """
 
 
+def verify_perimeter(grid, f, c):
+    """
+    Function to verify perimeter
+    """
+    perimeter = 0
+    filas = len(grid)
+    columnas = len(grid[0])
+    perimeter = 0
+    if grid[f][c] == 1:
+        if f - 1 >= 0 and grid[f - 1][c] == 0:
+            perimeter += 1
+        if c + 1 < columnas and grid[f][c + 1] == 0:
+            perimeter += 1
+        if f + 1 < filas and grid[f + 1][c] == 0:
+            perimeter += 1
+        if c - 1 >= 0 and grid[f][c - 1] == 0:
+            perimeter += 1
+    return perimeter
+
+
 def island_perimeter(grid):
     """
     returns the perimeter of the island described in grid
@@ -16,13 +36,6 @@ def island_perimeter(grid):
 
     for f in range(filas):
         for c in range(columnas):
-            if grid[f][c] == 1:
-                if f - 1 >= 0 and grid[f - 1][c] == 0:
-                    perimeter += 1
-                if c + 1 < columnas and grid[f][c + 1] == 0:
-                    perimeter += 1
-                if f + 1 < filas and grid[f + 1][c] == 0:
-                    perimeter += 1
-                if c - 1 >= 0 and grid[f][c - 1] == 0:
-                    perimeter += 1
+            perimeter += verify_perimeter(grid, f, c)
+
     return perimeter
